e9f56faed884d3954649c775a1e129f6d6abb0a0
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-09-12 21:44:17 +0700

    complete UI sign-in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -4,17 +4,22 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "@/constants";
 import { FormField } from "@/components/form-field";
+import CustomButton from "@/components/custom-button";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+  const [submitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
           <Image
             source={images.logo}
             resizeMode="contain"
@@ -38,6 +43,23 @@ const SignIn = () => {
             otherStyles="mt-7"
             keyboardType="email-address"
           />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={submitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
--- components/form-field.tsx
+++ components/form-field.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+import { icons } from "@/constants";
 
 interface FormFieldProps {
   title: string;
@@ -9,6 +11,7 @@ interface FormFieldProps {
   otherStyles: string;
   keyboardType: string;
 }
+
 export const FormField = ({
   title,
   value,
@@ -22,15 +25,25 @@ export const FormField = ({
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100">{title}</Text>
-      <View className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-2xl focus:border-secondary items-center">
+      <View className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-2xl focus:border-secondary items-center flex-row">
         <TextInput
-          className="flex-1 text-white font-psemibold text-base w-full"
+          className="flex-1 text-white font-psemibold text-base"
           value={value}
           placeholder={placeholder}
           placeholderTextColor="#7b7b8b"
           onChangeText={handleChangeText}
           secureTextEntry={title === "Password" && !showPassword}
         />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
